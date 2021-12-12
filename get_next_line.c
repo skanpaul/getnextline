149838a947293e3babd721e8f52f5e4b7fb85cbd
sorakann <sorakann.ki@gmail.com>
2021-12-12 21:05:22 +0100

    SUCCESS text1 et text2

--- get_next_line.c
+++ get_next_line.c
@@ -11,148 +11,58 @@
 /* ************************************************************************** */
 #include "get_next_line.h"
 /* ************************************************************************** */
+
+/* ************************************************************************** */
 char	*get_next_line(int fd)
 {
-	// DECLARATION
 	char			*new;
+	char			*old;
+	char			*extract;
 	static char		buf[BUFFER_SIZE + 1];
-	static size_t	start = 0;
-	static size_t	end = 0;
-	ssize_t			rd_ret;
+	static ssize_t	start = 0;
+	ssize_t			end;
+	static ssize_t	char_read = 0;
+
+	new = ft_substr("", 0, 1);
+	buf[BUFFER_SIZE] = '\0';
+
+	// Est-ce que tout le buffer a été traité?
+	if (start == char_read)
+	{
+		start = 0;
+		char_read = read(fd, buf, BUFFER_SIZE);
+
+		if ((char_read == 0) || (char_read == -1)) // si fin de fichier ou problem
+		{
+			free (new);
+			return (NULL);
+		}
+	}
 	
-	// INITIALISATION
-	buf[BUFFER_SIZE]= '\0';
-	rd_ret = 0;	
-	
-	// A faire seulement si on finit de lire une ligne
-	new = ft_substr("", 0, 0);
+	// Cherche le caractère RETOUR-A-LA-LIGNE
+	end = start;
+	while ((buf[end] != 0x0A) && (buf[end] != '\0'))
+		end++;
 
-	// BOUCLE DU CODE B
-	rd_ret = code_b(fd, new, buf, &start, &end);
-	if ((rd_ret == -1) || (rd_ret == 0))
-		return (NULL);
-
-	// FAIRE CODE A
-	rd_ret = code_a(fd, new, buf, &start, &end);
-	if ((rd_ret == -1) || (rd_ret == 0))
-		return (NULL);
-	
-	return (new);
-}
-
-/* ************************************************************************** */
-/* CODE A: LECTURE --> BUFFER AVEC 1 OU PLUSIEURS FINS DE LIGNES ************ */
-/* ************************************************************************** */
-ssize_t code_a(int fd, char *new, char *buf, size_t *start, size_t *end)
-{
-	char *old;
-	char *extract;
-	ssize_t	rd_ret;
-	
-	// Remplir buffer, SI NECESSAIRE
-	rd_ret = fill_buf(fd, buf, start, end);
-	if ((rd_ret == -1) || (rd_ret == 0))
-		return (rd_ret);
-
-	// Préparation de l'index end
-	while ((buf[*end] != 0x0A) && (buf[*end] != '\0'))
-		(*end)++;
-
-	// Extraction de 
-	// 1) ligne ENTIÈRE OU
-	// 2) DEBUT de ligne OU 
-	// 3) FIN de ligne
-	extract = ft_substr(buf, *start, (*end - *start + 1));
+	// Extrait la ligne trouvée
+	extract = ft_substr(buf, start, (end - start));
 	if (!extract)
-		return (-1);
+	{
+		free (new);
+		return (NULL);
+	}
 
-	// Construction de la vrai ligne de texte
+	// Prépare le prochain START et END
+	if (end < char_read - 1)	// si pas encore fin du fichier ou buffer		
+		start = ++end;
+	else 
+		start = char_read;
+
+	// Renvoie la nouvelle chaine
 	old = new;
 	new = ft_strjoin(old, extract);
-	if (!new)
-		return (-1);
 	free (extract);
-	free (old);
+	free(old);
 
-	// Préparation du prochain "start" et "end"
-	if (buf[*end] == '\0')
-		*end = 0;
-	else
-		(*end)++;
-	*start = *end;
-
-	return (rd_ret);
+	return (new);	
 }
-
-/* ************************************************************************** */
-/* CODE B: LECTURE -->BUFFER SANS FIN DE LIGNE ****************************** */
-/* ************************************************************************** */
-ssize_t code_b(int fd, char *new, char *buf, size_t *start, size_t *end)
-{
-	char	*old;
-	ssize_t	rd_ret;
-
-	rd_ret = 1;
-	
-	// Remplir buffer, SI NECESSAIRE
-	rd_ret = fill_buf(fd, buf, start, end);
-	if ((rd_ret == -1) || (rd_ret == 0))
-		return (rd_ret);
-
-	// -----------------------------------
-	while (!have_endlinechar(buf))
-	{
-		old = new;
-		new = ft_strjoin(old, new);		
-		free(old);
-		*start = 0;
-		*end = 0;
-
-		rd_ret = read(fd, (void *)buf, BUFFER_SIZE);
-		if ((rd_ret == -1) || (rd_ret == 0))
-			return (rd_ret);
-	}
-	return (rd_ret);
-}
-/* ************************************************************************** */
-/*  */
-/* ************************************************************************** */
-char	*have_endlinechar(char *buffer)
-{
-	int	i;
-
-	i = 0;
-	while ((buffer[i] != 0x0A) && (buffer[i] != '\0'))
-		i++;
-	if (buffer[i] == 0x0A)
-		return (&buffer[i]);
-	return (NULL);
-}
-
-/* ************************************************************************** */
-/* GIVE NEXT END ************************************************************ */
-/* ************************************************************************** */
-/* size_t give_next_end(char *buf, size_t *start, size_t *end)
-{
-	*end = *start;
-	while (buf[*end] != 0x0A)
-		(*end)++;
-
-} */
-
-/* ************************************************************************** */
-/* REMPLIR LE BUFFER ******************************************************** */
-/* ************************************************************************** */
-ssize_t fill_buf(int fd, char *buf, size_t *start, size_t *end)
-{
-	ssize_t rd_ret;
-
-	if ((*start == 0) && (*end == 0)) /* si ANCIEN buffer completement lu alors: */
-	{
-		rd_ret = read(fd, (void *)buf, BUFFER_SIZE);
-		if ((rd_ret == -1) || (rd_ret == 0))
-			return (rd_ret);
-	}
-	return (rd_ret);
-}
-
--- get_next_line.h
+++ get_next_line.h
@@ -22,20 +22,14 @@
 # include <unistd.h>
 /* -------------------------------------------------------------------------- */
 char	*get_next_line(int fd);
-
-
-ssize_t fill_buf(int fd, char *buf, size_t *start, size_t *end);
-ssize_t code_a(int fd, char *new, char *buf, size_t *start, size_t *end);
-ssize_t code_b(int fd, char *new, char *buf, size_t *start, size_t *end);
-char	*have_endlinechar(char *buffer);
+int     is_buf_rd_all(ssize_t *start, ssize_t *char_read);
 
 /* -------------------------------------------------------------------------- */
-
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-size_t	ft_strlen(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
-void	*ft_memmove(void *dst, const void *src, size_t len);
-char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+
 /* ************************************************************************** */
 # endif
