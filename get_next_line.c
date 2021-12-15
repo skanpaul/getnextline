f79ad36f033cff2883555dea48f8e6f703ecde6a
sorakann iMac <ski@student.42lausanne.com>
2021-12-15 15:25:03 +0100

    retrait commentaire

--- get_next_line.c
+++ get_next_line.c
@@ -16,49 +16,23 @@ char	*get_next_line(int fd)
 	char			*line;
 	char			*new;			
 	char			*old;
-	// static char		buf[BUFFER_SIZE + 1];
 	static char		buf[BUFFER_SIZE];
-
 	static ssize_t	char_read = BUFFER_SIZE;
-
-	int				line_done; // 0: line NOT complete | 1: line complete 
-
-	line = NULL;
-	new = NULL;
-	old = NULL;
+	int				line_done;
 
 	if (fd < 0)
 		return (NULL);
-	// ---------------------------------------------------------
-	// buf[BUFFER_SIZE] = '\0';
-	// ---------------------------------------------------------
-
 	line_done = 0;
 	line = ft_substr("", 0, 0);
 	while (line_done == 0)
 	{
 		new = create_segment(fd, buf, &line_done, &char_read);	
-		// if (!new)
-		// {
-		// 	free (line);
-		// 	return (NULL);
-		// }
-		
-		// if ((new == NULL) && (char_read = 0) && (line[0] != '\0'))
 		if (new == NULL) 
 		{
-			if (char_read == 0)
-			{
-				if (line[0] != '\0')
-					return (line);
-				free (line);	
-				return NULL;	
-			}
-			else
-			{
-				free (line);
-				return (NULL);
-			}
+			if ((char_read == 0) && (line[0] != '\0'))
+				return (line);
+			free (line);	
+			return NULL;
 		}
 		else
 		{
@@ -70,7 +44,6 @@ char	*get_next_line(int fd)
 				return (NULL);
 		}
 	}
-
 	return (line);		
 }
 /* ************************************************************************** */
@@ -83,8 +56,6 @@ char *create_segment(int fd, char *buf, int *line_done, ssize_t *char_read)
 	static ssize_t	start = BUFFER_SIZE;
 	ssize_t			end;
 	
-	// ---------------------------------------------------------
-	// Est-ce que tout le buffer a été complètement analysé ?
 	if (start == *char_read)
 	{
 		start = 0;
@@ -92,23 +63,50 @@ char *create_segment(int fd, char *buf, int *line_done, ssize_t *char_read)
 		if ((*char_read == 0) || (*char_read == -1)) // si fin de fichier ou problem
 			return (NULL);
 	}
-	// Cherche la fin de segment pour le "new" string
 	end = start;
 	while ((buf[end] != 0x0A) && (end < (*char_read - 1)))
 		end++;
-
-	// Si RETOUR-A-LA-LIGNE trouvé
 	if (buf[end] == 0x0A)
-		*line_done = 1;	// Alors FIN-LIGNE trouvé 
-
-	// Extrait la ligne trouvée (complète ou tronqué)
-	new = ft_substr(buf, start, (end - start + 1));		// crée un "new" string 
-	
-	if (end >= *char_read - 1)	// SI:		tous les caractère du buffer ont été analysé
-		start = *char_read;		// ALORS:	prêt a remplir un nouveau le buffer	
+		*line_done = 1;	
+	new = ft_substr(buf, start, (end - start + 1));
+	if (end >= *char_read - 1)
+		start = *char_read;
 	else 
-		start = ++end;			// SINON:	préparte le prochain START et END
-	// ----------------------------------------------------------------------
+		start = ++end;
 	return (new);	
 }
 /* ************************************************************************** */
+// char *create_segment(int fd, char *buf, int *line_done, ssize_t *char_read) 
+// {
+// 	char			*new;
+// 	static ssize_t	start = BUFFER_SIZE;
+// 	ssize_t			end;
+	
+// 	// ---------------------------------------------------------
+// 	// Est-ce que tout le buffer a été complètement analysé ?
+// 	if (start == *char_read)
+// 	{
+// 		start = 0;
+// 		*char_read = read(fd, buf, BUFFER_SIZE);
+// 		if ((*char_read == 0) || (*char_read == -1)) // si fin de fichier ou problem
+// 			return (NULL);
+// 	}
+// 	// Cherche la fin de segment pour le "new" string
+// 	end = start;
+// 	while ((buf[end] != 0x0A) && (end < (*char_read - 1)))
+// 		end++;
+
+// 	// Si RETOUR-A-LA-LIGNE trouvé
+// 	if (buf[end] == 0x0A)
+// 		*line_done = 1;	// Alors FIN-LIGNE trouvé 
+
+// 	// Extrait la ligne trouvée (complète ou tronqué)
+// 	new = ft_substr(buf, start, (end - start + 1));		// crée un "new" string 
+	
+// 	if (end >= *char_read - 1)	// SI:		tous les caractère du buffer ont été analysé
+// 		start = *char_read;		// ALORS:	prêt a remplir un nouveau le buffer	
+// 	else 
+// 		start = ++end;			// SINON:	préparte le prochain START et END
+// 	// ----------------------------------------------------------------------
+// 	return (new);	
+// }
